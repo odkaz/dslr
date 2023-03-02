8a6e083cc896875876e8616821ddbf182428446b
odkaz <one.destiny.japan@gmail.com>
2023-03-02 16:34:52 +0100

    add:percentile

--- describe.py
+++ describe.py
@@ -32,6 +32,18 @@ def get_std(data):
     std_dev = math.sqrt(var)
     return std_dev
 
+def get_percentile(data, percent, key=lambda x:x):
+    if not data:
+        return None
+    k = (len(data) - 1) * percent
+    f = math.floor(k)
+    c = math.ceil(k)
+    if f == c:
+        return key(data[int(k)])
+    d0 = key(data[int(f)]) * (c - k)
+    d1 = key(data[int(c)]) * (k - f)
+    return d0 + d1
+
 # main
 def main():
     if len(sys.argv) != 2:
@@ -50,18 +62,42 @@ def main():
             feature.append(float(elem))
     # for elem in feature:
     #     print(elem)
+    feature.sort()
     result['count'] = len(feature)
     result['mean'] = get_mean(feature)
     result['std'] = get_std(feature)
+    result['min'] = feature[0]
+    result['25%'] = get_percentile(feature, percent=0.25)
+    result['50%'] = get_percentile(feature, percent=0.5)
+    result['75%'] = get_percentile(feature, percent=0.75)
+    result['max'] = feature[-1]
+    print('--------------')
+    print('count', result['count'])
     print('mymean', result['mean'])
     print('myvar', get_variance(feature))
     print('mystd', result['std'])
+    print('min', result['min'])
+    print('25%', result['25%'])
+    print('50%', result['50%'])
+    print('75%', result['75%'])
+    print('max', result['max'])
+    print('--------------')
     # result['']
     # data.sort(key=take_second)
     # util.show_data(data)
     #show answer
     s = pd.Series(feature)
     print(s.describe())
+    print('--------------')
 
 if __name__ == '__main__':
     main()
+count 1568
+mymean 39.79713089016489
+myvar 270710.28727294196
+mystd 520.2982676051708
+min -966.7405456412164
+25% -489.55138715041005
+50% 260.2894464246512
+75% 524.7719489693718
+max 1016.2119403873959
