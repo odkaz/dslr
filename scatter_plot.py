38677e4f652c02acf6a851d5d52ac19c0efd1a14
odkaz <one.destiny.japan@gmail.com>
2023-03-04 18:47:21 +0100

    update: show all pairs

--- scatter_plot.py
+++ scatter_plot.py
@@ -25,17 +25,18 @@ house_colors = {
 
 df = pd.read_csv('./datasets/dataset_train.csv')
 
-figure, axis = plt.subplots(12, 12)
+figure, axis = plt.subplots(13, 13)
 
 for i in range(0, 13):
-    for j in range(i + 1, 13):
+    for j in range(0, 13):
         for house in house_colors:
             x = df[df['Hogwarts House'] == house]
             y = df[df['Hogwarts House'] == house]
-            axis[i, j - 1].scatter(y[column_names[j]], x[column_names[i]], alpha=0.5, color=house_colors[house], label=house)
+            if (i != j):
+                axis[i, j].scatter(y[column_names[j]], x[column_names[i]], alpha=0.5, color=house_colors[house], label=house)
         if (i == 0):
-            axis[i, j - 1].set_title(column_names[j], pad=35)
-        if (j == i + 1):
+            axis[0, j].set_title(column_names[j], pad=35)
+        if (j == 0):
             axis[i, 0].set_ylabel(column_names[i], rotation=0, labelpad=90)
 
 plt.show()
