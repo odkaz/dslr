4c2981717bf7cf851a9422f4492e7352f4695839
odkaz <one.destiny.japan@gmail.com>
2023-03-17 14:05:55 +0100

    update:single plot

--- dslr.py
+++ dslr.py
@@ -24,8 +24,9 @@ house_colors = {
     'Ravenclaw': 'blue'
 }
 
-
-
+def sigmoid(z):
+    g = 1/ (1 + np.exp(-z))
+    return g
 
 # def estimate_price(bias, weight, x):
 #     y =  bias + weight * x
@@ -57,22 +58,54 @@ def data_normalisation(data):
 def get_train_data():
     df = pd.read_csv('./datasets/dataset_train.csv', index_col = 'Index')
     f1 = 'Herbology'
-    f2 = 'Defense Against the Dark Arts'
     # eliminate the row with nan
-    df = df.dropna(subset=[f1, f2])
+    df = df.dropna(subset=[f1])
+    x_train = data_normalisation(df[f1])
     y_train = []
     for index, row in df.iterrows():
         if (row['Hogwarts House'] == 'Gryffindor'):
             y_train.append(1)
         else:
             y_train.append(0) 
+    np_x = np.array(x_train)
+    np_y = np.array(y_train)
+    return np_x, np_y
 
-    df_f1 = data_normalisation(df[f1])
-    df_f2 = data_normalisation(df[f2])
+def plot_data(x_train, y_train):
+    # x_train = np.array(x_train)
+    # y_train = np.array(y_train)
+    pos = y_train == 1
+    neg = y_train == 0
 
-    return df_f1, df_f2, y_train
+    print('xtrain', x_train[pos])
+    print('xtrain', x_train[neg])
+    print('ytrain', y_train[pos])
+    print('ytrain', y_train[neg])
+    # fig,ax = plt.subplots(1,2,figsize=(8,3))
+    #plot 1, single variable
+    plt.scatter(x_train[pos], y_train[pos], marker='x', s=80, c = 'red', label="y=1")
+    plt.scatter(x_train[neg], y_train[neg], marker='o', s=100, label="y=0", c='blue',lw=3)
+    plt.show()
 
-def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+# def get_train_data_double():
+#     df = pd.read_csv('./datasets/dataset_train.csv', index_col = 'Index')
+#     f1 = 'Herbology'
+#     f2 = 'Defense Against the Dark Arts'
+#     # eliminate the row with nan
+#     df = df.dropna(subset=[f1, f2])
+#     y_train = []
+#     for index, row in df.iterrows():
+#         if (row['Hogwarts House'] == 'Gryffindor'):
+#             y_train.append(1)
+#         else:
+#             y_train.append(0) 
+
+#     df_f1 = data_normalisation(df[f1])
+#     df_f2 = data_normalisation(df[f2])
+
+#     return df_f1, df_f2, y_train
+
+def plot_data_double(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
     """ plots logistic data with two axis """
     # Find Indices of Positive and Negative Examples
     pos = y == 1
@@ -86,19 +119,14 @@ def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
     ax.legend(loc=loc)
 
 def logistic_regression():
-    df_f1, df_f2, y_train = get_train_data()
+    x_train, y_train = get_train_data()
     learning_rate = 0.001
     iterate = 100000
     theta0, theta1 = 0,0
 
-
-    list_of_lists = list(zip(df_f1, df_f2))
-    fig,ax = plt.subplots(1,1,figsize=(4,4))
-    X = np.array(list_of_lists)
-    y = np.array(y_train).reshape(-1,1) 
-    plot_data(X, y, ax)
+    plot_data(x_train, y_train)
     # plt.scatter(list_of_lists, y_train)
-    plt.show()
+    # plt.show()
     return theta0, theta1
 
 def main():
